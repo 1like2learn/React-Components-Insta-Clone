cd9f28e80c9cc98f1fe7c2a9c1f4071c17c5f8fd
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-06-09 12:13:52 -0700

    Posts tied to PostsPage

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,12 +4,19 @@ import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from '../../dummy-data.js'
+console.log(dummyData)
 
 const PostsPage = () => {
   // set up state for your data
+  const [users, setUsers] = useState(dummyData)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {users.map((user) =>{
+        return <Post key ={user.username} user={user}/>
+        
+      })}
     </div>
   );
 };
